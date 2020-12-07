358d6b07e1df3f021c78dd733b7c8d678f2acf31
denil.parmar <Denil.Parmar@vwcredit.com>
2020-12-07 09:43:32 -0500

    firt commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import store from './store/store';
+import { addUsers } from './actions/users';
+import Header from './components/Header';
+import { Provider } from 'react-redux';
+import UsersList from './components/UsersList';
+import './index.css';
+
+class App extends React.Component {
+  componentDidMount() {
+    axios.get('http://localhost:3000/users').then(res => {
+      console.log(res.data);
+      store.dispatch(addUsers(res.data.results));
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <UsersList />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
